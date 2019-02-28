ef01cfca078cd9388dfc8b72237edb742fb42d9f
Roger <rdpender3@gmail.com>
2019-02-27 21:41:42 -0800

    taking out extra parts

--- client/src/pages/Books/Books.js
+++ client/src/pages/Books/Books.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import DeleteBtn from "../../components/DeleteBtn";
+import Nav from "../../components/Nav";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
@@ -24,6 +25,7 @@ class Books extends Component {
   render() {
     return (
       <Container fluid>
+        <Nav />
         <Row>
           <Col size="md-6">
             <Jumbotron>
